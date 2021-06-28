4be8d5ce348ad1ed52a107b6eb2dec0cd6e4db0e
justinthelaw <justinwingchunglaw@gmail.com>
2021-06-28 14:18:37 -0700

    toUpperCase bind

--- Week_8/sql_tables_intro/src/02-insert-rows.sql
+++ Week_8/sql_tables_intro/src/02-insert-rows.sql
@@ -1,8 +1,9 @@
-INSERT INTO list VALUES ('milk', 2, 'gallons');
-INSERT INTO list VALUES ('eggs', 1, 'dozen');
-INSERT INTO list VALUES ('butter', 12, 'sticks');
-INSERT INTO list VALUES ('flour', 1, 'pound');
-INSERT INTO list VALUES ('sour cream', 1, 'dollop');
-INSERT INTO list VALUES ('apples', 2, 'bushels');
-INSERT INTO list VALUES ('sugar', 2, 'pounds');
-INSERT INTO list VALUES ('oj', 3, 'gallons');
+INSERT INTO list (item, amount, units) VALUES
+  ('milk', 2, 'gallons'),
+  ('eggs', 1, 'dozen'),
+  ('butter', 12, 'sticks'),
+  ('flour', 1, 'pound'),
+  ('sour cream', 1, 'dollop'),
+  ('apples', 2, 'bushels'),
+  ('sugar', 2, 'pounds'),
+  ('oj', 3, 'gallons');
